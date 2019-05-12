2d5c63180b43a233fb6229a4ee71c7300af1eb5b
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2019-05-11 23:28:58 -0300

    add server & basic multiplayer connection

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const socket = require("socket.io")(http);
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static("client"));
+
+app.get("/", (req, res) => {
+  res.sendfile(__dirname + "/client/index.html");
+});
+
+socket.on("connection", socket => {
+  console.log("user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+
+
+  socket.on('ship-move', (direction) => {
+    console.log('ship move to: '+ direction);
+    socket.broadcast.emit('ship-move', direction);
+  })
+});
+
+http.listen(port, () => {
+  console.log("Space invader game listening of port " + port);
+});
